a6dcac1d1b69cbcd37ab80a65d7a8373a8080b7a
Gabriel de Andrade <gabriel.andrade@codata.pb.gov.br>
2025-04-05 21:23:30 -0300

    first commit: project creation

---
+++ src/main/java/com/example/gtasks/GtasksApplication.java
@@ -0,0 +1,13 @@
+package com.example.gtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gtasks/GtasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
